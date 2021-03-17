0b77ce90ebbcc69e727b332d2bbec40dadea44ab
Frank Spin <info@frankspin.nl>
2021-03-17 11:10:29 +0100

    update contact

--- themes/philflo/assets/js/cms/collections/forms.js
+++ themes/philflo/assets/js/cms/collections/forms.js
@@ -32,7 +32,7 @@ const fields = [
     label: "Form Type",
     name: "type",
     widget: "select",
-    options: ["quotation", "contact", "birthday", "review_intern", "review_extern", "30minutes"],
+    options: ["quotation", "contact", "birthday", "review_intern", "review_extern", "30minutes", "newsletter"],
     required: true,
     i18n: true,
   },
